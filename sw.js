e11672bc65773411e450b9dcaf2928d8ce057a06
SidArtDesignLab <mmaheshverma1@gmail.com>
2026-02-09 08:53:42 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "valuemirror-v1";
+const CACHE_NAME = "valuemirror-v2";
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) =>
